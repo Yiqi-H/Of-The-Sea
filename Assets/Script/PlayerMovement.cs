7b10513163b6c68f4289d4582ce22c9e58d7b94c
Yiqi-H <144740550+Yiqi-H@users.noreply.github.com>
2024-03-05 21:30:51 +0200

    3.5

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 200;
+    Vector2 move;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = move * speed * Time.deltaTime;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        move = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+    }
+}
