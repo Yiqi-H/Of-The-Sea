092573d17cb2f7c1c91e0821fcd8b6fd44bb5586
Haseeb Mubashir <145267131+haseeb68@users.noreply.github.com>
2024-04-21 14:51:58 +0500

    Sorting_Assets

---
+++ Assets/Game_Data/GameScripts/Script/MainMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    string SceneName;
+    public GameObject Loading;
+    public Text PercentageText;
+    public int i = 0;
+  
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    public void LoadScene(string _scenename)
+    {
+        SceneName = _scenename;
+        //Loading.GetComponent<Loading>().LoadLevel(SceneName);
+       // StartCoroutine(PercentageCalculator());
+        LoadSceneInvoke();
+        //Invoke("LoadSceneInvoke", 2);
+        //Invoke("LoadSceneInvoke", 4);
+        //SceneManager.LoadScene(scenename);
+    }
+
+    public void LoadSceneInvoke()
+    {
+
+        SceneManager.LoadScene(SceneName);
+
+    }
+    //public IEnumerator PercentageCalculator()
+    //{
+        
+    //    yield return new WaitForSeconds(0.03f);
+    //    i += 1;
+    //    if (i >= 101)
+    //    {
+    //        i = 100;
+
+    //        StopAllCoroutines();
+    //        LoadSceneInvoke();
+    //    }
+    //    else
+    //    {
+
+    //        StartCoroutine(PercentageCalculator());
+
+    //    }
+    //    PercentageText.text = "" + i + " %";
+    //}
+}
