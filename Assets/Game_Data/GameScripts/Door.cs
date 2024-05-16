f355092e6deadd3214903ca2578c2807fc57f641
haseeb68 <haseebmubashir21@gmail.com>
2024-05-16 13:33:33 +0300

    DoorComments

--- Assets/Game_Data/GameScripts/Door.cs
+++ Assets/Game_Data/GameScripts/Door.cs
@@ -8,22 +8,19 @@ public class Door : MonoBehaviour
     
     private void OnTriggerStay2D(Collider2D collision)
     {
+        // Check if the colliding object is the player and the player does not have the Level1 key
         if (collision.gameObject.CompareTag("Player") && !Inventory.Level1key)
         {
-
             MessageBox.SetActive(true);
         }
     }
 
-
     private void OnTriggerExit2D(Collider2D collision)
     {
+        // Check if the object exiting the trigger is the player
         if (collision.gameObject.CompareTag("Player"))
         {
-
             MessageBox.SetActive(false);
         }
     }
-
-
 }
